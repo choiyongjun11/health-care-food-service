2b89c4e8244e1a120fe942bc4ceb0268438e5b23
choiyongjun11 <ppigimi10@gmail.com>
2025-03-21 15:34:15 +0900

    git actions docker hub 연동(수정3)

--- src/main/java/com/example/healthcare/demo/DemoController.java
+++ src/main/java/com/example/healthcare/demo/DemoController.java
@@ -1,4 +1,14 @@
 package com.example.healthcare.demo;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+    @GetMapping("/")
+    public String test() {
+        return "Hello World~~!";
+
+    }
 }
